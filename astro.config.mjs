69c50e68200ba7f28cf0e745aa29990036745b70
CPK <cpkreu@gmail.com>
2025-01-17 11:44:34 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'PixiEditor Docs',
+			social: {
+				github: 'https://github.com/PixiEditor/PixiEditor',
+			},
+			sidebar: [
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
+				{
+					label: 'Getting Started 🏠',
+					autogenerate: { directory: 'getting-started' },
+				},
+				{
+					label: 'Open Beta 🚀',
+					autogenerate: { directory: 'open-beta' },
+				},
+				{
+					label: 'Usage 🎮',
+					autogenerate: { directory: 'usage' },
+				},
+				{
+					label: 'Contributing 💻',
+					autogenerate: { directory: 'contribution' },
+				},
+				{
+					label: 'Color Picker 🎨',
+					autogenerate: { directory: 'color-picker' },
+				},
+				{
+					label: 'Other 🔍',
+					autogenerate: { directory: 'other' },
+				},
+			],
+		}),
+	],
+});
