86671b7af6639c9125474c3b1dfed9b77d97ba01
CPK <cpkreu@gmail.com>
2025-01-17 12:44:59 +0000

    Use Theme colors from starlights theme builder

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,15 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 // Generated color palettes
+const accent = { 200: '#f2b7b7', 600: '#a4002b', 900: '#5b141d', 950: '#3f1317' };
+const gray = { 100: '#f5f6f8', 200: '#eceef2', 300: '#c0c2c7', 400: '#888b96', 500: '#545861', 700: '#353841', 800: '#24272f', 900: '#17181c' };
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: { accent, gray },
 		},
 	},
 	plugins: [starlightPlugin()],
