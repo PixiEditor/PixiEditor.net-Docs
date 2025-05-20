63c2814c230ed752e825b8c647d62feeb60b1442
Krzysztof Krysiński <flubbet@gmail.com>
2025-05-20 10:52:53 +0200

    Important configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,56 +2,70 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'PixiEditor Docs',
-        social: {
-            github: 'https://github.com/PixiEditor/PixiEditor',
-        },
-        customCss: [
-            './src/tailwind.css'
-        ],
-        sidebar: [
-            // {
-            // 	label: 'Guides',
-            // 	items: [
-            // 		// Each item here is one entry in the navigation menu.
-            // 		{ label: 'Example Guide', slug: 'guides/example' },
-            // 	],
-            // },
-            // {
-            // 	label: 'Reference',
-            // 	autogenerate: { directory: 'reference' },
-            // },
-            {
-                label: 'Getting Started 🏠',
-                autogenerate: { directory: 'getting-started' },
-            },
-            {
-                label: 'Open Beta 🚀',
-                autogenerate: { directory: 'open-beta' },
-            },
-            {
-                label: 'Usage 🎮',
-                autogenerate: { directory: 'usage' },
-            },
-            {
-                label: 'Contributing 💻',
-                autogenerate: { directory: 'contribution' },
-            },
-            {
-                label: 'Color Picker 🎨',
-                autogenerate: { directory: 'color-picker' },
-            },
-            {
-                label: 'Other 🔍',
-                autogenerate: { directory: 'other' },
-            },
-        ],
-		}), tailwind({
-            applyBaseStyles: false
-        })],
+  site: 'https://pixieditor.net',
+  base: '/docs/',
+
+  integrations: [starlight({
+      title: 'PixiEditor Docs',
+      editLink: {
+          baseUrl: "https://github.com/PixiEditor/PixiEditor.net-Docs/tree/main",
+      },
+      social: [
+          { icon: 'github', label: 'GitHub', href: 'https://github.com/PixiEditor/PixiEditor' },
+          { icon: 'discord', label: 'Discord', href: "https://discord.gg/qSRMYmq" }
+      ],
+      logo: {
+          light: './src/assets/logo-light.svg',
+          dark: './src/assets/logo-dark.svg',
+          replacesTitle: true
+      },
+      customCss: [
+          './src/styles/global.css'
+      ],
+      sidebar: [
+          // {
+          // 	label: 'Guides',
+          // 	items: [
+          // 		// Each item here is one entry in the navigation menu.
+          // 		{ label: 'Example Guide', slug: 'guides/example' },
+          // 	],
+          // },
+          // {
+          // 	label: 'Reference',
+          // 	autogenerate: { directory: 'reference' },
+          // },
+          {
+              label: 'Getting Started 🏠',
+              autogenerate: { directory: 'getting-started' },
+          },
+          {
+              label: 'Open Beta 🚀',
+              autogenerate: { directory: 'open-beta' },
+          },
+          {
+              label: 'Usage 🎮',
+              autogenerate: { directory: 'usage' },
+          },
+          {
+              label: 'Contributing 💻',
+              autogenerate: { directory: 'contribution' },
+          },
+          {
+              label: 'Color Picker 🎨',
+              autogenerate: { directory: 'color-picker' },
+          },
+          {
+              label: 'Other 🔍',
+              autogenerate: { directory: 'other' },
+          },
+      ],
+      })],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
